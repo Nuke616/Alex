d00e9227e7a46150fe7c8c2876fa7368bdf4fcb3
Никита <nuke581@gmain.com>
2023-10-07 20:41:19 +0300

    Я создал новую программу

--- helooo/src/Main.java
+++ helooo/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+    System.out.println("введите число");
+    int gg = new Scanner(System.in).nextInt();
+    System.out.println("введите число другое блядь");
+    int gg1 = new Scanner(System.in).nextInt();
+    System.out.println(gg *gg1 );
     }
 }
